49b30be0a7087d25c53fe9507bc06184fdcb7120
Rishitha1309 <59167143+Rishitha1309@users.noreply.github.com>
2020-06-25 10:09:02 -0700

    Create Ladies.java

---
+++ Milestone1/src/com/wipro/abstractclass/Ladies.java
@@ -0,0 +1,15 @@
+package com.wipro.abstractclass;
+
+public class Ladies extends Compartment {
+
+	 
+
+	@Override
+
+	public void notice() {
+
+		System.out.println("This is LADIES CLASS");
+
+	} 
+
+}
