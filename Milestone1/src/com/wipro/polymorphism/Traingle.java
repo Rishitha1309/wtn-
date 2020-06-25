2f61fcc39db8246e0cbee6265b82d428577f795c
Rishitha1309 <59167143+Rishitha1309@users.noreply.github.com>
2020-06-25 09:41:03 -0700

    Create Traingle.java

---
+++ Milestone1/src/com/wipro/polymorphism/Traingle.java
@@ -0,0 +1,11 @@
+package com.wipro.polymorphism;
+public class Traingle extends Shape{
+	void draw()
+	{
+		System.out.println("Drawing Triangle");
+	}
+	void erase()
+	{
+		System.out.println("Erasing Traingle");
+	}
+}
