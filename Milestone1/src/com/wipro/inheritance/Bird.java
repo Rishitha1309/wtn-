293f7303066f12a0ee3cf5e932d90686a75c381e
Rishitha1309 <59167143+Rishitha1309@users.noreply.github.com>
2020-06-25 09:48:37 -0700

    Create Bird.java

---
+++ Milestone1/src/com/wipro/inheritance/Bird.java
@@ -0,0 +1,21 @@
+package com.wipro.inheritance;
+
+public class Bird extends Animal{
+	public void sleep() {
+
+	    System.out.println("A bird sleeps.");
+
+	}
+
+	public void eats() {
+
+	    System.out.println("A bird eats.");
+
+	}
+
+	public void fly() {
+
+	    System.out.println("A bird flies.");
+
+	}
+}
