0caf0a69ef9f6d09d934bb078fb1dc7bb27f7290
Rishitha1309 <59167143+Rishitha1309@users.noreply.github.com>
2020-06-25 09:49:14 -0700

    Create Inher1.java

---
+++ Milestone1/src/com/wipro/inheritance/Inher1.java
@@ -0,0 +1,23 @@
+package com.wipro.inheritance;
+
+public class Inher1 {
+	public static void main(String [] args)
+
+	{
+
+		Animal obj1=new Animal();
+
+		Bird obj2=new Bird();
+
+		obj1.sleep();
+
+		obj1.eats();
+
+		obj2.sleep();
+
+		obj2.eats();
+
+		obj2.fly();
+
+	}
+}
