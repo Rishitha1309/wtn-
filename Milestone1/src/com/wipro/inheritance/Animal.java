269acf61f4e7f66a68b8cf20786678d97ec23851
Rishitha1309 <59167143+Rishitha1309@users.noreply.github.com>
2020-06-25 09:48:03 -0700

    Create Animal.java

---
+++ Milestone1/src/com/wipro/inheritance/Animal.java
@@ -0,0 +1,15 @@
+package com.wipro.inheritance;
+
+public class Animal {
+	public void sleep() {
+
+	    System.out.println("An animal sleeps.");
+
+	}
+
+	public void eats() {
+
+	    System.out.println("An animal eats.");
+	    }
+
+}
