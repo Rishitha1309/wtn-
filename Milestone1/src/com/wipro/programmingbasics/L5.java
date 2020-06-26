ab5cb2c0e66f3cedf189cb768041086eaaf9ff6e
Rishitha1309 <59167143+Rishitha1309@users.noreply.github.com>
2020-06-25 19:45:21 -0700

    Create L5.java

---
+++ Milestone1/src/com/wipro/programmingbasics/L5.java
@@ -0,0 +1,17 @@
+package com.wipro.programmingbasics;
+
+public class L5 {
+	public static void main(String args[])
+    {
+        String s=args[0];
+        char temp=s.charAt(0);
+        if(temp>=48&&temp<=57)
+            System.out.println(temp+" is a number");
+        else if(temp>='a'&&temp<='z'||temp>='A'&&temp<='Z')
+            System.out.println(temp+" is an alphabet");
+        else
+            System.out.println(temp+" is a special character");
+    }
+
+
+}
