ac3879c68b445f5148a51d55127f88de8ab02a4e
Rishitha1309 <59167143+Rishitha1309@users.noreply.github.com>
2020-06-25 20:00:05 -0700

    Create P2.java

---
+++ Milestone1/src/patterns/P2.java
@@ -0,0 +1,20 @@
+package patterns;
+import java.util.Scanner;
+
+public class P2 {
+	public static void main(String args[])
+	{
+		Scanner sc= new Scanner(System.in);
+		int rows= sc.nextInt();
+		for(int i=0;i<rows;i++)
+		{
+			for(int j=0;j<i;j++)
+			{
+				System.out.println("*");
+			}
+			System.out.println();
+		}
+		sc.close();
+
+}
+}
