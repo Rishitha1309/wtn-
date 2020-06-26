d6d6b4fff36e3d5687e4e5006f77cbc267e15c28
Rishitha1309 <59167143+Rishitha1309@users.noreply.github.com>
2020-06-25 19:37:36 -0700

    Create C3.java

---
+++ Milestone1/src/com/wipro/programmingbasics/C3.java
@@ -0,0 +1,11 @@
+package com.wipro.programmingbasics;
+
+public class C3 {
+	public static void main(String args[])
+	{
+	 int x= Integer.parseInt(args[0]);
+	 int y= Integer.parseInt(args[1]);
+	 int z=x+y;
+	 System.out.println("The sum of "+x+" and "+y+" is: "+z);
+	}
+}
