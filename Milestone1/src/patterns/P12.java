bbd72b7fad2232d5b06248220d5d9f7d1743aaa8
Rishitha1309 <59167143+Rishitha1309@users.noreply.github.com>
2020-06-25 20:02:34 -0700

    Create P12.java

---
+++ Milestone1/src/patterns/P12.java
@@ -0,0 +1,20 @@
+package patterns;
+import java.util.Scanner;
+
+public class P12 {
+	public static void main(String args[])
+	{
+		Scanner sc= new Scanner(System.in);
+		int rows= sc.nextInt();
+		for(int i=1;i<=rows;i++)
+		{
+			for(int j=rows;j>=i;j--)
+			{
+				System.out.print(""+j);
+			}
+			System.out.println();
+		}
+		sc.close();
+
+}
+}
